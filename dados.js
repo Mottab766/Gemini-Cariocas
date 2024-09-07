2f279894f1179ce8c8c0348cc9409ce888a63d0e
Bruno Motta <100230939+Mottab766@users.noreply.github.com>
2024-09-07 11:04:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados  = 
+[{
+    titulo :"Clube de Regatas Vasco da Gama",
+    imagem : "./imagens_times/vasco.png",
+    descricao : "O Club de Regatas Vasco da Gama, fundado em 1898, é um dos clubes mais tradicionais e populares do Brasil. Nascido como um clube de remo, em homenagem ao navegador português Vasco da Gama, o Vasco rapidamente se destacou no cenário esportivo nacional, principalmente no futebol. <br><br> Com uma história marcada por lutas contra o preconceito e a busca por igualdade, o Gigante da Colina conquistou diversos títulos e construiu uma torcida apaixonada e fiel, que o acompanha em todos os cantos do país. Sua trajetória é um reflexo da própria história do Brasil, marcada por momentos de glória e desafios.",
+    link : "https://vasco.com.br/linha-do-tempo/",
+    botaotime : "Clique para saber mais sobre o Gigante da colina!",
+    tags: "gigante colina barreira portugues português"
+
+},{
+    titulo :"Clube de Regatas do Flamengo",
+    imagem : "./imagens_times/fla.png",
+    descricao : "O Clube de Regatas do Flamengo, fundado em 1895, é um dos clubes mais populares do Brasil, conhecido mundialmente por sua paixão e tradição. Com uma das maiores torcidas do mundo, o Flamengo conquistou diversos títulos nacionais e internacionais, tornando-se um dos maiores clubes do futebol brasileiro. Sua história é marcada por grandes jogadores e momentos históricos que o transformaram em um verdadeiro símbolo do esporte no país.",
+    link : "https://www.flamengo.com.br/institucional/historia",
+    botaotime : "Veja mais sobre o Mengão!",
+    tags: "nação nacao mengão mengao mengo"
+
+},{
+    titulo:"Fluminense Futebol Clube",
+    imagem : "./imagens_times/flu.png",
+    descricao: "O Fluminense Football Club, fundado em 1902, é um dos clubes mais tradicionais do Rio de Janeiro e do Brasil. Conhecido como Tricolor, o clube possui uma vasta história marcada por grandes jogadores e títulos importantes. Sua torcida apaixonada e fiel o acompanha em todos os momentos, vibrando pelas conquistas do time.",
+    link: "https://www.fluminense.com.br/sobre/a-historia",
+    botaotime : "Veja mais sobre o Time de Guerreiros!",
+    tags: "nense nence guerreiros"
+},{
+    titulo:"Botafogo de Futebol e Regatas",
+    imagem : "./imagens_times/bot.png",
+    descricao: "O Botafogo de Futebol e Regatas, fundado em 1893, é um dos clubes mais tradicionais do Rio de Janeiro. Conhecido como Glorioso, o clube possui uma vasta história marcada por grandes jogadores e títulos importantes. Sua torcida apaixonada e fiel o acompanha em todos os momentos, vibrando pelas conquistas do time. ",
+    link: "https://www.botafogo.com.br/historia",
+    botaotime : "Clique para saber mais sobre o Fogão!",
+    tags: "fogão fogao fogo"
+
+
+},{
+        titulo:"América Futebol Clube",
+        imagem : "./imagens_times/america.png",
+        descricao: "O América Futebol Clube, fundado em 1904, é um dos clubes mais tradicionais do Rio de Janeiro. Conhecido como Mecão, o clube possui uma vasta história marcada por grandes jogadores e títulos importantes. Sua torcida apaixonada e fiel o acompanha em todos os momentos, vibrando pelas conquistas do time. ",
+        link: "https://americario.com.br/fundacao/#",
+        botaotime : "Clique para saber mais sobre o Mecão!",
+        tags: "america mecão mecao"
+
+}];
+
+
